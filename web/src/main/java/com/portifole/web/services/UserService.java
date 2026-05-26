c8855864a4dab36536b2c3903acd783cec166fb8
Deividy Silva <deividy.silva2015@gmail.com>
2026-05-26 17:18:47 -0300

    Service layer, component registration

---
+++ web/src/main/java/com/portifole/web/services/UserService.java
@@ -0,0 +1,26 @@
+package com.portifole.web.services;
+
+import com.portifole.web.entities.User;
+import com.portifole.web.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll(){
+        return repository.findAll();
+    }
+
+    public User findById(Long id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+}
